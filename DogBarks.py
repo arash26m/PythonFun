ebd0d5ca399477b1a634f5783cc64581c01c66bc
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2020-02-22 09:28:48 -0800

    Count Dog Barks
    Problem:
    Suppose > is a dog moving in left to right direction and < is a dog moving in right to left direction. Each dog barks once meet other dogs. Write a Python code to count the number of barks for the following scenario:
    <<->--<--<-<

---
+++ DogBarks.py
@@ -0,0 +1,16 @@
+# Problem question:
+# Suppose > is a dog moving in left to right direction and < is a dog moving in right to left direction. Each dog barks once meet other dogs. 
+# Write a Python code to count the number of barks for the following scenario.
+# <<->--<--<-<
+
+a=list("<<->--<--<-<")
+count=0
+b=0
+for i in a:
+    b=b+1
+    if i == '>':
+        l = len(a)-b
+        for j in range (l):
+            if a[j+b] == '<':
+                count = count + 1
+print(count*2)
