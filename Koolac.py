be1c88ef1e4541ee009928de161a6eff87ef6c0d
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2021-06-10 10:24:02 -0700

    Update Koolac.py

--- Koolac.py
+++ Koolac.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 x=[1,2,3]; y=[1,2,3]; z=[1,2,3]
 plt.axes(projection="3d").scatter3D(x,y,z,color="blue",s=100)
 plt.show()
+
+#Function time
+def f():
+    for i in range(10**8):
+        pass
+
+from time import time
+start=time()
+f()
+end=time()
+
+print("duration: ",start-end)
