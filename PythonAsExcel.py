115a0b675b3a368610c66c6440f5887275e6c006
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2021-06-25 18:58:09 -0700

    Update PythonAsExcel.py

--- PythonAsExcel.py
+++ PythonAsExcel.py
@@ -7,10 +7,12 @@
 import pandas as pd
 df=pd.DataFrame({'a' : [1 , 2, 3], 'b' : [4, 5, 6]})
 df.head()
+
 #add rows under data frame
 df2=pd.DataFrame({'a':[7, 8], 'b':[9,10]})
 df2.head()
 df=df.append(df2)
+
 #calculate sum of column a
 df['a'].sum()
 
@@ -23,15 +25,18 @@ rows, columns = df.shape
 df.head(2)
 df.tail()
 df.D1
+
 #add 2 columns
 D3=df.D1+df.D2
 print(D3)
 D4=D3*df.D1
 print(D4)
+
 #add new column to df
 df['D3'] = D3 
 df ['D4'] = D4
 df
+
 #delete or drop column
 df.drop(['D3','D4'], axis=1)
 df
@@ -39,9 +44,11 @@ df.drop(['D3'], axis=1, inplace=True)
 df
 del df['D4']
 df
+
 #drop or delete row by index
 df.drop([0, 1, 2])
 df
+
 #set index for a column
 df
 df.set_index('D0', inplace=True)
@@ -50,9 +57,11 @@ df.drop(['b', 'a'],inplace=True)
 df
 df.reset_index(inplace=True)
 df
+
 #import data again
 df=pd.read_csv("C:/Users/ataghi2/Desktop/Data.csv")
 df
+
 #show specific columns
 df[['D1','D2']]
 
@@ -81,3 +90,32 @@ df
 x=df[(df.D=='yes')&(df.D0=='a')&(df.D1>10)]
 x
 x.D2.sum()
+
+#-------------------------------Excel V-LookUp
+import pandas as pd
+import numpy as np
+
+df1=pd.read_excel('C:/Users/ataghi2/Desktop/R/Python/1.xlsx')
+df2=pd.read_excel('C:\\Users\\ataghi2\\Desktop\\R\\Python\\2.xlsx')
+df1
+df2
+print(df1.columns)
+print(df2.columns)
+
+#rename column
+df1.rename(columns={'no':'ID'}, inplace=True)
+df1
+
+#v-lookup
+df3=pd.merge(df1, df2[['ID','Gift']], on='ID', how='left')
+print(df3)
+
+#replace nan with nothing
+df3=df3.replace(np.nan, '', regex=True)
+print(df3)
+
+#write in excel
+df3.to_excel('C:/Users/ataghi2/Desktop/R/Python/3.xlsx', index=False)
+
+#-------------------------------
+
