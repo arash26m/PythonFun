d8fae25b7266ebea31f3f3b2e289c5cdd0edf594
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2022-07-18 10:13:04 -0700

    Create PythonAsExcel.py

--- PythonAsExcel.py
+++ PythonAsExcel.py
@@ -3,7 +3,7 @@
 #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.html#https://www.ritchieng.com/pandas-multi-criteria-filtering/
 #https://www.ritchieng.com/pandas-multi-criteria-filtering/
 
-#make a data frame
+#-------------------------------make a data frame
 import pandas as pd
 df=pd.DataFrame({'a' : [1 , 2, 3], 'b' : [4, 5, 6]})
 df.head()
@@ -12,40 +12,36 @@ df.head()
 df2=pd.DataFrame({'a':[7, 8], 'b':[9,10]})
 df2.head()
 df=df.append(df2)
-
-#calculate sum of column a
+#-------------------------------calculate sum of column a
 df['a'].sum()
 
 #import csv data
 import pandas as pd
 df=pd.read_csv("C:/Users/ataghi2/Desktop/Data.csv")
 
-#number of rows and columns
+#-------------------------------number of rows and columns
 rows, columns = df.shape
 df.head(2)
 df.tail()
 df.D1
 
-#add 2 columns
+#-------------------------------add 2 columns
 D3=df.D1+df.D2
 print(D3)
 D4=D3*df.D1
 print(D4)
-
-#add new column to df
+#-------------------------------add new column to df
 df['D3'] = D3 
 df ['D4'] = D4
 df
-
-#delete or drop column
+#-------------------------------delete or drop column
 df.drop(['D3','D4'], axis=1)
 df
 df.drop(['D3'], axis=1, inplace=True)
 df
 del df['D4']
 df
-
-#drop or delete row by index
+#-------------------------------drop or delete row by index
 df.drop([0, 1, 2])
 df
 
@@ -57,24 +53,20 @@ df.drop(['b', 'a'],inplace=True)
 df
 df.reset_index(inplace=True)
 df
-
-#import data again
+#-------------------------------import data again
 df=pd.read_csv("C:/Users/ataghi2/Desktop/Data.csv")
 df
-
-#show specific columns
+#-------------------------------show specific columns
 df[['D1','D2']]
 
 #show specific rows and columns
 df.loc[2:4,['D1','D2']]
 df.loc[df.D0=='b',['D1','D2']]
-
-#show statistics
+#-------------------------------show statistics
 df.D1.max()
 df.D1.mean()
 df.D1.describe()
-
-#filter by condition
+#-------------------------------filter by condition
 df
 x=df[df.D1>=50]
 x
@@ -90,7 +82,6 @@ df
 x=df[(df.D=='yes')&(df.D0=='a')&(df.D1>10)]
 x
 x.D2.sum()
-
 #-------------------------------Excel V-LookUp
 import pandas as pd
 import numpy as np
@@ -101,12 +92,10 @@ df1
 df2
 print(df1.columns)
 print(df2.columns)
-
-#rename column
+#-------------------------------rename column
 df1.rename(columns={'no':'ID'}, inplace=True)
 df1
-
-#v-lookup
+#-------------------------------v-lookup
 df3=pd.merge(df1, df2[['ID','Gift']], on='ID', how='left')
 print(df3)
 
@@ -147,4 +136,18 @@ for i in range(len(D)):
     E.append(D[i]+C[i])
     
 print('E= ', E)
-    
+#-------------------------------Make data categorical
+
+
+
+
+
+
+
+
+
+
+
+
+
+
