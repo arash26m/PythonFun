2ed366c1109a8aa1d60394b46db470285209435b
Arash Taghinezhad <155667922+ataghinezhadbilandi@users.noreply.github.com>
2024-05-29 11:39:03 -0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -133,10 +133,33 @@ class Person:
 
 age = 24
 is_adult = Person.is_adult(age)
-#-----------------------------------Class &
+#-----------------------------------Class & classmethod
+#in class method we can define new variables insde the class
+from datetime import date
 
-
-
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+ 
+    # a class method to create a Person object by birth year.
+    @classmethod
+    def fromBirthYear(cls, name, year):
+        return cls(name, date.today().year - year)
+ 
+    # a static method to check if a Person is adult or not.
+    @staticmethod
+    def isAdult(age):
+        return age > 18
+ 
+person1 = Person('mayank', 21)
+person2 = Person.fromBirthYear('mayank', 1996)
+ 
+print(person1.age)
+print(person2.age)
+ 
+# print the result
+print(Person.isAdult(22))
 
 #---------------------------- Import csv file
 df=pd.read_csv("C:/Users/ataghi2/Desktop/Data.csv")
